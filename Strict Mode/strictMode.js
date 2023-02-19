6a953abca7f8ede82d850fbb53c92ac603cdc8b6
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-02-20 00:12:48 +0530

    strict mode

---
+++ Strict Mode/strictMode.js
@@ -0,0 +1,11 @@
+// strict mode = with the strict mode you can not use the variable without defining it. Always use the strict mode at the top of the file 'use strict'
+
+// without strict mode
+
+// y = 5;   // no error
+// console.log(y); //
+
+'use strict'
+
+x = 4   //
+console.log(x)
