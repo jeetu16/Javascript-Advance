feb6bdae1ed43b7c526aba40f65c6c3ef448df45
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-02-21 07:59:32 +0530

    Hoisting in JS

---
+++ Hoisting/hoisting.js
@@ -0,0 +1,17 @@
+// Hoisting = Uses function and variables before defining or initialzing
+
+
+// function hoisting
+// hello();   
+// function hello() {
+//     console.log("hello world!");
+// }
+
+
+// variable hoisting with 'var'
+// console.log(x); // undefined
+// var x;
+
+// variable hoisting with 'let'
+console.log(y) // error
+let y = 10;
